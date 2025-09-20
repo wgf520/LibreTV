001102d6789971dda1aa5f0e755d1654fdfdbaa1
wgf520 <128127036+wgf520@users.noreply.github.com>
2025-09-20 11:34:43 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,15 +2,15 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
-    }
+    },
     ftys: {
         api: 'https://www.fantuan.tv/api.php/provide/vod',
         name: '饭团影视',
-    }
+    },
     ysgc: {
         api: 'https://cj.lziapi.com/api.php/provide/vod',
         name: '影视工厂',
-    }     
+    }   
 };
 
 // 调用全局方法合并
